ab9bc22fbd3ecba0a8e30d2be0d7f30f92b6bfb1
Diukarev Sergii <vmesteprotiv@gmail.com>
2016-08-25 12:34:06 +0300

    add method of authorization

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -8,74 +10,106 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 class GetThread extends Thread {
-	private int n;
+    private int n;
 
-	@Override
-	public void run() {
-		try {
-			while (!isInterrupted()) {
-				URL url = new URL("http://localhost:2408/get?from=" + n);
-				HttpURLConnection http = (HttpURLConnection) url.openConnection();
+    @Override
+    public void run() {
+        try {
+            while (!isInterrupted()) {
+                URL url = new URL("http://localhost:2408/get?from=" + n);
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
 
-				InputStream is = http.getInputStream();
-				try {
-					int sz = is.available();
-					if (sz > 0) {
-						byte[] buf = new byte[is.available()];
-						is.read(buf);
+                try (InputStream is = http.getInputStream()) {
+                    int sz = is.available();
+                    if (sz > 0) {
+                        byte[] buf = new byte[is.available()];
+                        is.read(buf);
 
-						Gson gson = new GsonBuilder().create();
-						Message[] list = gson.fromJson(new String(buf), Message[].class);
+                        Gson gson = new GsonBuilder().create();
+                        Message[] list = gson.fromJson(new String(buf), Message[].class);
 
-						for (Message m : list) {
-							System.out.println(m);
-							n++;
-						}
-					}
-				} finally {
-					is.close();
-				}
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			return;
-		}
-	}
+                        for (Message m : list) {
+                            System.out.println(m);
+                            n++;
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
+        }
+    }
 }
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		try {
-			System.out.println("Enter login: ");
-			String login = scanner.nextLine();
-	
-			GetThread th = new GetThread();
-			th.setDaemon(true);
-			th.start();
 
-			while (true) {
-				String text = scanner.nextLine();
-				if (text.isEmpty())
-					break;
+    private static Scanner scanner = new Scanner(System.in);
 
-				Message m = new Message();
-				m.setText(text);
-				m.setFrom(login);
+    private static String authorization() {
+        System.out.println("Please log in:");
 
-				try {
-					int res = m.send("http://localhost:2408/add");
-					if (res != 200) {
-						System.out.println("HTTP error: " + res);
-						return;
-					}
-				} catch (IOException ex) {
-					System.out.println("Error: " + ex.getMessage());
-					return;
-				}
-			}
-		} finally {
-			scanner.close();
-		}
-	}
+        String login = "";
+
+        while (true) {
+            System.out.println("\tEnter login:");
+            login = scanner.nextLine();
+            System.out.println("\tEnter password: ");
+            String pass = scanner.nextLine();
+
+            try {
+                URL url = new URL(String.format("http://localhost:2408/authorization?login=%s&pass=%s", login, pass));
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                if (connection.getResponseCode() == 200) {
+                    break;
+                } else {
+                    System.out.println("Incorrect login or password!!!");
+                    System.out.println("Do you want try again? (Y / N)");
+                    if (!scanner.nextLine().equalsIgnoreCase("y")) {
+                        System.exit(0);
+                    }
+                }
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println();
+        }
+
+        return login;
+    }
+
+    public static void main(String[] args) {
+        String login = authorization();
+
+        GetThread th = new GetThread();
+        th.setDaemon(true);
+        th.start();
+
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.isEmpty())
+                break;
+
+            Message m = new Message();
+            m.setText(text);
+            m.setFrom(login);
+
+            try {
+                int res = m.send("http://localhost:2408/add");
+                if (res != 200) {
+                    System.out.println("HTTP error: " + res);
+                    return;
+                }
+            } catch (IOException ex) {
+                System.out.println("Error: " + ex.getMessage());
+                return;
+            }
+        }
+    }
 }
--- src/Message.java
+++ src/Message.java
@@ -7,78 +7,74 @@ import java.util.Date;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class Message implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class Message {
 
-	private Date date = new Date();
-	private String from;
-	private String to;
-	private String text;
-	
-	public String toJSON() {
-		Gson gson = new GsonBuilder().create();
-		return gson.toJson(this);
-	}
-	
-	public static Message fromJSON(String s) {
-		Gson gson = new GsonBuilder().create();
-		return gson.fromJson(s, Message.class);
-	}
-	
-	@Override
-	public String toString() {
-		return new StringBuilder().append("[").append(date.toString())
-				.append(", From: ").append(from).append(", To: ").append(to)
-				.append("] ").append(text).toString();
-	}
+    private Date date = new Date();
+    private String from;
+    private String to;
+    private String text;
 
-	public int send(String url) throws IOException {
-		URL obj = new URL(url);
-		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
-		
-		conn.setRequestMethod("POST");
-		conn.setDoOutput(true);
-	
-		OutputStream os = conn.getOutputStream();
-		try {
-			String json = toJSON();
-			os.write(json.getBytes());
-			
-			return conn.getResponseCode();
-		} finally {
-			os.close();
-		}
-	}
-	
-	public Date getDate() {
-		return date;
-	}
+    public String  toJSON() {
+        Gson gson = new GsonBuilder().create();
+        return gson.toJson(this);
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public static Message fromJSON(String s) {
+        Gson gson = new GsonBuilder().create();
+        return gson.fromJson(s, Message.class);
+    }
 
-	public String getFrom() {
-		return from;
-	}
+    @Override
+    public String toString() {
+        return new StringBuilder().append("[").append(date.toString())
+                .append(", From: ").append(from).append(", To: ").append(to)
+                .append("] ").append(text).toString();
+    }
 
-	public void setFrom(String from) {
-		this.from = from;
-	}
+    public int send(String url) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
 
-	public String getTo() {
-		return to;
-	}
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
 
-	public void setTo(String to) {
-		this.to = to;
-	}
+        try (OutputStream os = conn.getOutputStream()) {
+            String json = toJSON();
+            os.write(json.getBytes());
 
-	public String getText() {
-		return text;
-	}
+            return conn.getResponseCode();
+        }
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
