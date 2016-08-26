74eaf5e454155c0ad272121bcb42e895b4912377
Diukarev Sergii <vmesteprotiv@gmail.com>
2016-08-26 16:58:31 +0300

    add status_en.properties

--- src/chat/client/command/StatusCommand.java
+++ src/chat/client/command/StatusCommand.java
@@ -1,5 +1,6 @@
 package chat.client.command;
 
+import chat.client.Chat;
 import chat.client.ConsoleHelper;
 import chat.client.exception.InterruptOperationException;
 
@@ -7,25 +8,25 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
 public class StatusCommand implements Command {
-    //TODO
+    private ResourceBundle res
+            = ResourceBundle.getBundle(Chat.RESOURCE_PATH + "status_en");
 
     @Override
     public void execute() throws InterruptOperationException {
-        ConsoleHelper.writeMessage("Please, type the nickname below: ");
+        ConsoleHelper.writeMessage(res.getString("type"));
         String login = ConsoleHelper.readString();
-        HttpURLConnection connection = null;
         try {
-            URL url = new URL(String.format("http://localhost:2408/status?login=%s", login));
-            connection = (HttpURLConnection) url.openConnection();
+            URL url = new URL(String.format(res.getString("address"), login));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             if (connection.getResponseCode() == 200){
-                ConsoleHelper.writeMessage(String.format("\tUser \"%s\" is online", login));
+                ConsoleHelper.writeMessage(String.format(res.getString("online"), login));
             } else {
-                ConsoleHelper.writeMessage(String.format("\tUser \"%s\" is offline", login));
+                ConsoleHelper.writeMessage(String.format(res.getString("offline"), login));
             }
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
