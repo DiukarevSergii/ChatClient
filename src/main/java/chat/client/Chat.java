ecca840cf7533adbfe0bf175f54295537e76592a
Diukarev Sergii <vmesteprotiv@gmail.com>
2016-09-01 12:05:27 +0300

    problem

--- src/main/java/chat/client/Chat.java
+++ src/main/java/chat/client/Chat.java
@@ -3,23 +3,30 @@ package chat.client;
 import chat.client.command.CommandExecutor;
 import chat.client.exception.InterruptOperationException;
 
+import java.io.File;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Chat {
 
-    public static final String RESOURCE_PATH = "chat.client.resources.";
+    public static String RESOURCE_PATH = "resources.";
+
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
-        try {
-            Operation operation;
-            CommandExecutor.execute(Operation.LOGIN);
-            do {
-                operation = ConsoleHelper.askOperation();
-                CommandExecutor.execute(operation);
-            } while (!operation.equals(Operation.EXIT));
-        } catch (InterruptOperationException e) {
-            ConsoleHelper.printExitMessage();
-        }
+        ResourceBundle rb = ResourceBundle.getBundle("resources.exit_en");
+
+        System.out.println(rb);
+
+//        try {
+//            Operation operation;
+//            CommandExecutor.execute(Operation.LOGIN);
+//            do {
+//                operation = ConsoleHelper.askOperation();
+//                CommandExecutor.execute(operation);
+//            } while (!operation.equals(Operation.EXIT));
+//        } catch (InterruptOperationException e) {
+//            ConsoleHelper.printExitMessage();
+//        }
     }
 }
